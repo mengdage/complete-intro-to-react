e87345721c2ae85c0ee532279e13005a8e835464
meng <wulalameng@gmail.com>
2017-09-14 10:41:18 -0400

    use JSX

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,25 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { render } from 'react-dom';
 
-const MyTitle = props =>
-  React.createElement('div', null, React.createElement('h1', { style: { color: props.color } }, props.title));
-const MyFirstComponent = () =>
-  React.createElement(
-    'div',
-    { id: 'my-first-component' },
-    React.createElement(MyTitle, {
-      title: 'Game of Thrones 3',
-      color: 'YellowGreen'
-    }),
-    React.createElement(MyTitle, {
-      title: 'Stranger Things',
-      color: 'GreenYellow'
-    }),
-    React.createElement(MyTitle, { title: 'Rick and Morty', color: '#123' }),
-    React.createElement(MyTitle, {
-      title: 'Silicon Valley',
-      color: 'rgb(255, 0, 0)'
-    })
-  );
+const MyTitle = props => (
+  <div>
+    <h1 style={{ color: props.color }}>{props.title}</h1>
+  </div>
+);
+
+MyTitle.propTypes = {
+  title: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired
+};
+
+const MyFirstComponent = () => (
+  <div id="my-first-component">
+    <MyTitle title="Game of Thrones" color="YellowGreen" />
+    <MyTitle title="Stranger Things" color="GreenYellow" />
+    <MyTitle title="Rick and Morty" color="#123" />
+    <MyTitle title="Silicon Valley" color="rgb(255, 0,0 )" />
+  </div>
+);
 
 render(React.createElement(MyFirstComponent), document.getElementById('app'));
